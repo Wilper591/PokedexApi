5a0a19e94fadd5cfa57a136b878ea963e8e39327
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-03-01 15:10:15 -0300

    Se añade funcion para buscar pokemon por id

--- assets/js/controllers/controllers.js
+++ assets/js/controllers/controllers.js
@@ -1,15 +1,15 @@
 let URL_BASE = "https://pokeapi.co/api/v2/pokemon";
 /* let pokemonName = "charmander"; */
-export const getPokemons = async () => {
+export const getPokemons = async (id) => {
   try {
-    let response = await fetch(`${URL_BASE}`);
+    let response = await fetch(`${URL_BASE}/${id}`);
     let data = await response.json();
-    return data;
+    const pokemon = {
+      nombre: data.name,
+      img: data.sprites.other.dream_world.front_default
+    };
+    return pokemon;
   } catch (error) {
     console.log(error);
   }
 };
-
-getPokemons();
-
-
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,25 @@
 import { getPokemons } from "./controllers/controllers.js";
 const cardSection = document.querySelector("#card-section");
-
-const mostrarCard = async () => {
-  const data = await getPokemons();
-  const pokemons = data.results;
-  const nextPageable = data.next;
-  const PreviousPageable = data.previous;
+const inputPokemon = document.querySelector("#inputId");
+const btnPokemon = document.querySelector("#btnBuscar");
+btnPokemon.addEventListener("click", (e) => {
+  e.preventDefault();
+  mostrarCard(inputPokemon.value);
+});
+const mostrarCard = async (id) => {
+  const data = await getPokemons(id);
+  const pokemons = data;
+  console.log(pokemons);
+  /*   const nextPageable = next;
+  const PreviousPageable = previous;
   console.log(nextPageable)
   console.log(PreviousPageable);
-  console.log(pokemons);
+  console.log(pokemons.sprites); */
   // Hacer un innerHTML para el cardSection para insertar los card de cada pokemon
   let pokemonCards = "";
-  pokemons.forEach(
-    (pokemon) =>
-      (pokemonCards += `
-        <div>
-            <p> ${pokemon.name}</p>
-            <a href="${pokemon.url}">Link a detalles</a>
-        </div>`)
-  );
-
+  pokemonCards += `<div>
+            <p class="text-danger fs-1"> ${pokemons.nombre}</p>
+            <img class="image-fluid" src="${pokemons.img}">
+        </div>`;
   cardSection.innerHTML = pokemonCards;
 };
-mostrarCard();
