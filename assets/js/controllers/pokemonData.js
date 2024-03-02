8c96dd423a687ad973d6498b5ac2ea24f8681436
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-03-02 15:04:54 -0300

    Se Añaden comprobacion IF en caso que consultar un ruta url para una imagen con valor null

--- assets/js/controllers/pokemonData.js
+++ assets/js/controllers/pokemonData.js
@@ -3,9 +3,20 @@ export const getPokemons = async (id) => {
   try {
     let response = await fetch(`${URL_BASE}/${id}`);
     let data = await response.json();
+    let imgUrl = "";
+    
+    if (data.sprites.other.dream_world.front_default !== null) {
+      imgUrl = data.sprites.other.dream_world.front_default;
+    } else if (data.sprites.other["official-artwork"].front_default !== null) {
+      imgUrl = data.sprites.other["official-artwork"].front_default;
+    } else {
+      imgUrl = data.sprites.other.home.front_default;
+    }
+
+
     const pokemon = {
       nombre: data.name,
-      img: data.sprites.other.dream_world.front_default,
+      img: imgUrl,
       tipo: data.types.map(tipos => tipos.type.name),
     };
 /*     console.log(pokemon); */
