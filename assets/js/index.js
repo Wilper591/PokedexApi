e0352f29e58a3a35fb225327675bde2fd40b051b
Jorge Cerda <jorge_cerda_591@hotmail.com>
2024-03-04 13:58:52 -0300

    Cambiando estilos, se carga v1.3.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,8 +5,8 @@ const allCardsSection = document.querySelector("#allPokeCards");
 const inputPokemon = document.querySelector("#inputId");
 const btnPokemon = document.querySelector("#btnBuscar");
 btnPokemon.addEventListener("click", (e) => {
-  e.preventDefault(); 
-  mostrarCard((inputPokemon.value).toLowerCase());
+  e.preventDefault();
+  mostrarCard(inputPokemon.value.toLowerCase());
 });
 
 /* Funcion para mostrar Card al buscar en el input */
@@ -16,28 +16,28 @@ const mostrarCard = async (id) => {
   let pokemonCards = "";
   let estadisticasPokemon = "";
   let PokeTipo = "";
+  /* Iterador de tipos de pokemon */
+  for (let i = 0; i < pokemons.tipo.length; i++) {
+    PokeTipo += `<p id="tipoText" class="text-capitalize">${pokemons.tipo[i]}</p>`;
+  }
   /* Iterador de estadisticas pokemon */
   for (let i = 0; i < pokemons.estadisticasName.length; i++) {
-    estadisticasPokemon += 
-    `<p class="text-uppercase">
+    estadisticasPokemon += `<p class="text-uppercase">
         ${pokemons.estadisticasName[i]}: ${pokemons.estadisticasValor[i]}
     </p>`;
   }
-  for (let i = 0; i < pokemons.tipo.length; i++) {
-    PokeTipo += `${pokemons.tipo[i]} `;
-  }
   pokemonCards += `
     <div id="inputCard" class="text-center">
-          <p class="fs-3 text-capitalize border-bottom border-3"> ${pokemons.nombre}</p>
-            <img id="imgInput" class="container m-1" src="${pokemons.img}">
-          <div id="contenedorTipo" class="border-top border-3">
+        <p class="fs-3 text-capitalize border-bottom border-3"> ${pokemons.nombre}</p>
+          <img id="imgInput" class="container m-1" src="${pokemons.img}">
+        <div class="border-top border-3">
           <p id="pokeIDInput">N°: ${pokemons.id}</p>
-          <p id="inputTipo" class="text-capitalize">Tipo: ${PokeTipo} </p>
-          </div>
-      </div>
-      <div id="statsCard">
-        <p class="fs-1 border-bottom border-3">Estadísticas</p>
-        ${estadisticasPokemon}
+          <h2 id="inputTipo" class="text-capitalize">tipo: ${PokeTipo}</h2 >
+        </div>
+    </div>
+    <div id="statsCard">
+      <p class="fs-1 border-bottom border-3">Estadísticas</p>
+      ${estadisticasPokemon}
     </div`;
   cardSection.innerHTML = pokemonCards;
 };
@@ -46,24 +46,24 @@ const mostrarCard = async (id) => {
 (async () => {
   const idPokemones = 48;
   let pokemonAllImg = "";
+  /* Iterador de ID en base a idPokemones */
   for (let id = 1; id <= idPokemones; id++) {
     const data = await getPokemons(id);
     const pokemonsName = data.nombre;
     const pokemonsImg = data.img;
     const pokemonsTipo = data.tipo;
     const pokemonsID = data.id;
-    let PokeTipo = "";
+    let PokeTipos = "";
+    /* Iterador de tipos de pokemon */
     for (let i = 0; i < pokemonsTipo.length; i++) {
-      PokeTipo += `${pokemonsTipo[i]} `;
+      PokeTipos += `<p id="tipoAllText" class="text-capitalize">${pokemonsTipo[i]}</p>`;
     }
     pokemonAllImg += `
     <div id="pokeCard" class="fs-2">
-      <img id="imgCard" class="container m-2 p-2" src="${pokemonsImg}">
-      <p id="nombrePoke" class="fw-bold text-capitalize border-bottom border-3">${pokemonsName}</p>
-      <div>
-        <p id="pokeIDAll" class="">N°: ${pokemonsID}</p>
-        <p id="idTipoText" class="text-capitalize">Tipo: ${PokeTipo}</p>
-      </div>
+      <img id="imgAllCard" class="container m-2 p-2" src="${pokemonsImg}">
+      <p class="fs-3 text-capitalize border-bottom border-3">${pokemonsName}</p>
+      <p id="pokeIdAll" class="">N°: ${pokemonsID}</p>
+      <h2 id="tipoAll" class="text-capitalize">tipo: ${PokeTipos}</h2>
     </div>`;
     allCardsSection.innerHTML = pokemonAllImg;
   }
